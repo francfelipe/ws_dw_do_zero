bf0974d88a70a4deaa83debe2d3de583a013a4bd
Felipe Francisco <fsfrancisco@gmail.com>
2024-06-21 23:50:00 -0300

    feat: adicionar feature de extract e load

--- src/extract_load.py
+++ src/extract_load.py
@@ -7,9 +7,25 @@ import os
 
 # import das minhas variaveis de ambiente
 
-commodities = ['CL=F', 'GC=F', 'SI=F']
+# definir os ativos que eu quero buscar
+commodities   = ['CL=F', 'GC=F', 'SI=F']
 
-def buscar_dados_commodities(simbolo, periodo='5y', intervalo='1d'):
+# carregar as variaveis de ambiente
+load_dotenv()
+DB_HOST       = os.getenv('DB_HOST_PROD')
+DB_PORT       = os.getenv('DB_PORT_PROD')
+DB_NAME       = os.getenv('DB_NAME_PROD')
+DB_USER       = os.getenv('DB_USER_PROD')
+DB_PASS       = os.getenv('DB_PASS_PROD')
+DB_SCHEMA     = os.getenv('DB_SCHEMA_PROD')
+
+# criar a string de conexão com o banco de dados
+DATABASE_PROD = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+# criar a conexão com o banco de dados
+engine = create_engine(DATABASE_PROD)
+
+def buscar_dados_commodities(simbolo, periodo='10y', intervalo='1d'):
     '''
     Função para buscar os dados de commodities
     Args:
@@ -18,7 +34,7 @@ def buscar_dados_commodities(simbolo, periodo='5y', intervalo='1d'):
         intervalo (str): intervalo de busca
     '''
     # realiza a consula
-    ticker = yf.Ticker('CL=F')
+    ticker = yf.Ticker(simbolo)
     # busca os dados
     dados = ticker.history(period=periodo, interval=intervalo)[['Close']]
     # insere a coluna simbolo
@@ -36,9 +52,12 @@ def buscar_todos_dados_commodities(commodities):
         todos_dados.append(dados)
     return pd.concat(todos_dados)
 
+def salvar_no_postgres(df, schema=DB_SCHEMA):
+    df.to_sql('commodities', con=engine, schema=schema, if_exists='replace', index=True, index_label='Date')
+
 if __name__ == '__main__':
     dados_concatenados = buscar_todos_dados_commodities(commodities)
-    print(dados_concatenados)
+    salvar_no_postgres(dados_concatenados, schema=DB_SCHEMA)
     
     
 # concatenar os meus ativos (1... 2... 3) -> (1)
